96a2283c7bf681f6fd709ecabaacbc1ac7720add
cgc <1099899538@qq.com>
2021-01-26 13:41:38 +0800

    在activity怎么获取fragment中的view？

--- app/src/main/java/com/cgc/sx/MainActivity.java
+++ app/src/main/java/com/cgc/sx/MainActivity.java
@@ -10,6 +10,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -20,23 +21,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavHostFragment navHostFragment =(NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController=navHostFragment.getNavController();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-        mMapView=(MapView)findViewById(R.id.bmapView);
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
-//        mMapView.onResume();
+//        mMapView=(MapView) findViewById(R.id.bmapView);
+        mMapView.onResume();
     }
     @Override
     protected void onPause() {
--- app/src/main/java/com/cgc/sx/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/cgc/sx/ui/dashboard/DashboardViewModel.java
@@ -4,13 +4,17 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.baidu.mapapi.map.MapView;
+
 public class DashboardViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private MapView mMapView;
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
+
     }
 
     public LiveData<String> getText() {
